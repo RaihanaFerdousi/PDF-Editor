48b0f4d5051efed80c63f7c7ad3b260ac6694cd4
Mashrufa2410 <mashrufa3006@gmail.com>
2026-03-31 10:34:48 +0600

    Home page Ui

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,8 @@ import {
   ScrollRestoration,
 } from "react-router";
 
+
+
 import type { Route } from "./+types/root";
 import "./app.css";
 
@@ -20,6 +22,10 @@ export const links: Route.LinksFunction = () => [
   {
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+  },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap",
   },
 ];
 
