d2c8cae36d736623e482ea273f61f40bcd89b24f
Mashrufa2410 <mashrufa3006@gmail.com>
2026-04-01 14:28:36 +0600

    installed pdf-lib

--- app/routes/editor.tsx
+++ app/routes/editor.tsx
@@ -1,20 +1,20 @@
-import { Viewer, Worker } from "@react-pdf-viewer/core";
-import { useLocation } from "react-router";
-import Canvas from "~/Components/canvas";
+  import { Viewer, Worker } from "@react-pdf-viewer/core";
+  import { useLocation } from "react-router";
+  import Canvas from "~/Components/canvas";
 
-export default function Editor() {
-    const location = useLocation();
-    const fileUrl = location.state?.fileUrl;
+  export default function Editor() {
+      const location = useLocation();
+      const fileUrl = location.state?.fileUrl;
 
-    return (
-    <div style={{ position: 'relative' }}>
-      {fileUrl && (
-       <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
-          <Viewer fileUrl={fileUrl} />
-        </Worker>
-      )}
-      
-      <Canvas/>
-    </div>
-  );
-}
+      return (
+      <div style={{ position: 'relative' }}>
+        {fileUrl && (
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+            <Viewer fileUrl={fileUrl} />
+          </Worker>
+        )}
+        
+        <Canvas/>
+      </div>
+    );
+  }
