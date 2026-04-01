7f9967e41241b32905f1cffa7c51fe7ee093247f
RaihanaFerdousi <raihanaferdousi110@gmail.com>
2026-04-01 16:19:35 +0600

    Added Thumbnail

--- app/routes/editor.tsx
+++ app/routes/editor.tsx
@@ -1,23 +1,34 @@
 import { Viewer, Worker } from "@react-pdf-viewer/core";
+import { thumbnailPlugin } from "@react-pdf-viewer/thumbnail";
 import { useLocation } from "react-router";
 import Canvas from "~/Components/canvas";
+import "@react-pdf-viewer/thumbnail/lib/styles/index.css";
 
 export default function Editor() {
   const location = useLocation();
   const fileUrl = location.state?.fileUrl;
 
+  const plugin = thumbnailPlugin();
+  const { Thumbnails } = plugin;
+
   return (
-    <div className="bg-gray-50">
+    <div className="bg-gray-50 min-h-screen">
       <div className="pt-24 pb-12 px-4 md:px-8">
-        <div className="max-w-5xl mx-auto">
+        <div className="max-w-7xl mx-auto">
           {fileUrl && (
             <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
-              <div>
-                <div>a
-                  <Viewer 
-                    fileUrl={fileUrl} 
+              <div className="flex overflow-hidden">
+                <div className="w-48 bg-white rounded-l-md border-l-2 flex-shrink-0 h-[750px] border-gray-300 overflow-y-auto p-2">
+                  <Thumbnails />
+                </div>
+
+                <div className="flex-1 overflow-hidden relative">
+                  <Viewer
+                    fileUrl={fileUrl}
+                    plugins={[plugin]}
                   />
                 </div>
+
               </div>
             </Worker>
           )}
