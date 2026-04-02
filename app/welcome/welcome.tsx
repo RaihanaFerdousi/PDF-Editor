eacf2f329e00ef7f6a701c6af78ca5ff2662e9e6
RaihanaFerdousi <raihanaferdousi110@gmail.com>
2026-04-02 11:54:58 +0600

    Added toolbar

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -14,13 +14,13 @@ export function Welcome() {
   return (
     <main className="h-screen w-full flex items-center justify-center">
       <div className="flex flex-col items-center gap-10">
-        
+
         <h1 className="text-5xl tracking-wide text-black font-hand">
           Welcome to PDF Editor
         </h1>
 
         <div className="flex gap-6">
-          
+
           <label className="font-hand px-8 py-1 border-3 border-black text-black rounded-xl cursor-pointer bg-transparent hover:bg-black hover:text-white transition">
             Import PDF
             <input
@@ -31,7 +31,10 @@ export function Welcome() {
             />
           </label>
 
-          <button className="font-hand px-8 py-1 border-3 border-black text-black rounded-xl hover:bg-black hover:text-white transition">
+          <button
+            onClick={() => navigate('/editor')}
+            className="font-hand px-8 py-1 border-3 border-black text-black rounded-xl hover:bg-black hover:text-white transition"
+          >
             New PDF
           </button>
 
